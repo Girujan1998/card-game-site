3a7f3e4609517293bd780eb97ce0ca7770fa4f4d
Girujan Naguleswaran <girujan@Girujans-MacBook-Pro.local>
2022-03-01 19:53:05 -0500

    Temp change in App.js

--- card-game-site-app/src/App.js
+++ card-game-site-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>This is the card-game-site project</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
