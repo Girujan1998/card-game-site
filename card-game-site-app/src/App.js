105809a3fbca2979516a69eee96dd41ac9e56318
Girujan Naguleswaran <ngirujan@gmail.com>
2022-03-08 21:04:39 -0500

    Adding small change

--- card-game-site-app/src/App.js
+++ card-game-site-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>This is the card-game-site project.</h2>
+        <h2>This is the card-game-site project. Card Game Site Project.</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
